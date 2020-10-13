12f0df103f4885588b66d09c84b774e101f198b1
Uladzimir Paliukhovich <Uladzimir_Paliukhovich@epam.com>
2020-10-13 13:35:35 +0300

    Refactoring of models generator

--- lib/src/code_generators/swagger_models_generator.dart
+++ lib/src/code_generators/swagger_models_generator.dart
@@ -230,4 +230,156 @@ abstract class SwaggerModelsGenerator {
 
     return '''  $jsonKeyContent  final List<$typeName> ${SwaggerModelsGenerator.generateFieldName(propertyName)};''';
   }
+
+  String generateGeneralPropertyContent(
+      String propertyName,
+      String propertyKey,
+      String className,
+      List<DefaultValueMap> defaultValues,
+      Map<String, dynamic> val,
+      List<String> allEnumNames) {
+    var jsonKeyContent = "@JsonKey(name: '$propertyKey'";
+    var typeName = getParameterTypeName(className, propertyName, val);
+
+    final allEnumsNamesWithoutPrefix =
+        allEnumNames.map((e) => e.replaceFirst('enums.', '')).toList();
+
+    if (allEnumsNamesWithoutPrefix.contains(typeName)) {
+      typeName = 'enums.$typeName';
+    }
+
+    final unknownEnumValue =
+        generateUnknownEnumValue(allEnumNames, typeName, false);
+
+    jsonKeyContent += unknownEnumValue;
+
+    if (defaultValues
+        .any((DefaultValueMap element) => element.typeName == typeName)) {
+      final defaultValue = defaultValues.firstWhere(
+          (DefaultValueMap element) => element.typeName == typeName);
+      jsonKeyContent +=
+          ', defaultValue: ${generateDefaultValueFromMap(defaultValue)})\n';
+    } else {
+      jsonKeyContent += ')\n';
+    }
+
+    return '''  $jsonKeyContent  final $typeName ${SwaggerModelsGenerator.generateFieldName(propertyName)};''';
+  }
+
+  String generatePropertyContentByType(
+      Map<String, dynamic> propertyEntryMap,
+      String propertyName,
+      String propertyKey,
+      String className,
+      List<DefaultValueMap> defaultValues,
+      bool useDefaultNullForLists,
+      List<String> allEnumsNames) {
+    switch (propertyEntryMap['type'] as String) {
+      case 'array':
+        return generateListPropertyContent(propertyName, propertyKey, className,
+            propertyEntryMap, useDefaultNullForLists, allEnumsNames);
+        break;
+      case 'enum':
+        return generateEnumPropertyContent(
+            propertyName, className, allEnumsNames);
+        break;
+      default:
+        return generateGeneralPropertyContent(propertyName, propertyKey,
+            className, defaultValues, propertyEntryMap, allEnumsNames);
+    }
+  }
+
+  String generatePropertiesContent(
+      Map<String, dynamic> propertiesMap,
+      String className,
+      List<DefaultValueMap> defaultValues,
+      bool useDefaultNullForLists,
+      List<String> allEnumNames) {
+    if (propertiesMap == null) {
+      return '';
+    }
+
+    final results = <String>[];
+
+    for (var i = 0; i < propertiesMap.keys.length; i++) {
+      var propertyName = propertiesMap.keys.elementAt(i);
+      final propertyEntryMap =
+          propertiesMap[propertyName] as Map<String, dynamic>;
+      final propertyKey = propertyName;
+
+      exceptionWords.forEach((String exceptionWord) {
+        if (propertyName == exceptionWord) {
+          propertyName = '\$' + propertyName;
+        }
+      });
+
+      if (propertyEntryMap.containsKey('type')) {
+        results.add(generatePropertyContentByType(
+            propertyEntryMap,
+            propertyName,
+            propertyKey,
+            className,
+            defaultValues,
+            useDefaultNullForLists,
+            allEnumNames));
+      } else if (propertyEntryMap['\$ref'] != null) {
+        results.add(generatePropertyContentByRef(propertyEntryMap, propertyName,
+            propertyKey, className, allEnumNames));
+      } else if (propertyEntryMap['schema'] != null) {
+        results.add(generatePropertyContentBySchema(propertyEntryMap,
+            propertyName, propertyKey, className, allEnumNames));
+      } else {
+        results.add(generatePropertyContentByDefault(
+            propertyEntryMap, propertyName, allEnumNames));
+      }
+    }
+
+    return results.join('\n');
+  }
+
+  String genetateEnumFromJsonToJsonMethods(List<String> enumNames) {
+    return enumNames.map((e) => generateEnumFromJsonToJson(e)).join('\n');
+  }
+
+  String generateEnumFromJsonToJson(String enumName) {
+    final neededName = enumName.replaceFirst('enums.', '');
+    return '''
+String ${neededName.camelCase}ToJson(enums.$neededName ${neededName.camelCase}) {
+  return enums.\$${neededName}Map[${neededName.camelCase}];
+}
+
+enums.$neededName ${neededName.camelCase}FromJson(String ${neededName.camelCase}) {
+  return enums.\$${neededName}Map.entries
+      .firstWhere((element) => element.value == ${neededName.camelCase})
+      .key;
+}
+
+List<String> ${neededName.camelCase}ListToJson(
+    List<enums.$neededName> ${neededName.camelCase}) {
+  return ${neededName.camelCase}
+      .map((e) => enums.\$${neededName}Map[e])
+      .toList();
+}
+
+List<enums.$neededName> ${neededName.camelCase}ListFromJson(
+    List<String> ${neededName.camelCase}) {
+  return ${neededName.camelCase}
+      .map((e) => ${neededName.camelCase}FromJson(e))
+      .toList();
+}
+    ''';
+  }
+
+  String generateConstructorPropertiesContent(Map<String, dynamic> entityMap) {
+    if (entityMap == null) {
+      return '';
+    }
+
+    final generatedConstructorParameters = entityMap.keys.map((String key) {
+      final fieldName = SwaggerModelsGenerator.generateFieldName(key);
+      return '\t\tthis.$fieldName,';
+    }).join('\n');
+
+    return '{\n${generatedConstructorParameters.toString()}\n\t}';
+  }
 }
