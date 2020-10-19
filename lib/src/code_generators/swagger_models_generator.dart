6001340a2adf559d5ab19d5e8c6753675dbfb228
Uladzimir Paliukhovich <Uladzimir_Paliukhovich@epam.com>
2020-10-19 11:40:44 +0300

    Added ? symbol

--- lib/src/code_generators/swagger_models_generator.dart
+++ lib/src/code_generators/swagger_models_generator.dart
@@ -387,7 +387,7 @@ enums.$neededName ${neededName.camelCase}FromJson(String ${neededName.camelCase}
 
   return enums.\$${neededName}Map.entries
       .firstWhere((element) => element.value$toLowerCaseString == ${neededName.camelCase}$toLowerCaseString, orElse: () => null)
-      .key ?? enums.$neededName.swaggerGeneratedUnknown;
+      ?.key ?? enums.$neededName.swaggerGeneratedUnknown;
 }
 
 List<String> ${neededName.camelCase}ListToJson(
