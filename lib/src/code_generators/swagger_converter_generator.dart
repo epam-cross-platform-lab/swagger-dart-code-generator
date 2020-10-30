6325147112903c81737a67c75ecb7276a61e5c8f
Uladzimir Paliukhovich <Uladzimir_Paliukhovich@epam.com>
2020-10-30 16:03:44 +0300

    Implemented mapping for converter generator

--- lib/src/code_generators/swagger_converter_generator.dart
+++ lib/src/code_generators/swagger_converter_generator.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:swagger_dart_code_generator/src/code_generators/v2/swagger_models_generator_v2.dart';
 import 'package:swagger_dart_code_generator/src/definitions.dart';
+import 'package:swagger_dart_code_generator/src/extensions/string_extension.dart';
 import 'package:recase/recase.dart';
 
 class SwaggerConverterGenerator {
@@ -12,29 +13,55 @@ ${_generateModelsMapping(dartCode)}};''';
   }
 
   String _generateModelsMapping(String dartCode) {
-    final result = StringBuffer();
+    final result = <String>[];
     final dynamic map = jsonDecode(dartCode);
 
     final definitions = getDefinitions(map);
 
-    if (definitions == null) {
-      return '';
-    }
+    if (definitions != null) {
+      for (var i = 0; i < definitions.keys.length; i++) {
+        final key = definitions.keys.elementAt(i).toString();
 
-    for (var i = 0; i < definitions.keys.length; i++) {
-      final key = definitions.keys.elementAt(i).toString();
+        if (definitions[key]['enum'] != null) {
+          continue;
+        }
 
-      if (definitions[key]['enum'] != null) {
-        continue;
+        final validatedName =
+            SwaggerModelsGeneratorV2().getValidatedClassName(key);
+
+        result.add('\t$validatedName: $validatedName.fromJsonFactory,');
       }
-
-      final validatedName =
-          SwaggerModelsGeneratorV2().getValidatedClassName(key);
-
-      result.writeln('\t$validatedName: $validatedName.fromJsonFactory,');
     }
 
-    return result.toString();
+    final responses = getResponses(map);
+    if (responses != null) {
+      responses.keys.forEach((key) {
+        if (!result.contains(key)) {
+          final response = responses[key] as Map<String, dynamic>;
+
+          final content = response == null
+              ? null
+              : response['content'] as Map<String, dynamic>;
+
+          final firstContent = content == null
+              ? null
+              : content.entries?.first?.value as Map<String, dynamic>;
+
+          final schema = firstContent == null
+              ? null
+              : firstContent['schema'] as Map<String, dynamic>;
+
+          if (schema != null &&
+              content.entries.length == 1 &&
+              !schema.containsKey('\$ref')) {
+            final validatedName = key.capitalize;
+            result.add('\t$validatedName: $validatedName.fromJsonFactory,');
+          }
+        }
+      });
+    }
+
+    return result.join('\n');
   }
 
   Map<String, dynamic> getDefinitions(dynamic map) {
@@ -44,6 +71,14 @@ ${_generateModelsMapping(dartCode)}};''';
 
     if (map['components'] != null) {
       return map['components']['schemas'] as Map<String, dynamic>;
+    }
+
+    return null;
+  }
+
+  Map<String, dynamic> getResponses(dynamic map) {
+    if (map['components'] != null) {
+      return map['components']['responses'] as Map<String, dynamic>;
     }
 
     return null;
