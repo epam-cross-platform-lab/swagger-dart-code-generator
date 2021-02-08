15e21b1f198a564751f1dbdd3ae8616dcb7a2e3f
Uladzimir Paliukhovich <Uladzimir_Paliukhovich@epam.com>
2021-02-08 10:49:20 +0300

    Replaced 'material' to 'meta'

--- lib/src/code_generators/swagger_additions_generator.dart
+++ lib/src/code_generators/swagger_additions_generator.dart
@@ -68,7 +68,7 @@ import 'package:chopper/chopper.dart' as chopper;''';
 
     result.writeln("""
 import 'package:json_annotation/json_annotation.dart';
-import 'package:flutter/widgets.dart';""");
+import 'package:flutter/meta.dart';""");
 
     if (chopperImports.isNotEmpty) {
       result.write(chopperImports);
