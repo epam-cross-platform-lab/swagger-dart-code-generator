459d6ef16b02f8e28bb2f8a979ed4ddcc99dc950
VOVANELLA <vovanella95@mail.ru>
2021-08-26 18:51:20 +0300

    Added some tests for requests generator

---
+++ test/generator_tests/requests_generator_test.dart
@@ -0,0 +1,29 @@
+import 'package:swagger_dart_code_generator/src/code_generators/swagger_requests_generator.dart';
+import 'package:swagger_dart_code_generator/src/models/generator_options.dart';
+import 'package:test/test.dart';
+
+import 'test_data.dart';
+
+void main() {
+  group('Additions generator tests', () {
+    final generator = SwaggerRequestsGenerator();
+
+    test('Should generate CarsApi', () {
+      final result = generator.generate(
+        code: carsService,
+        className: 'CarsService',
+        fileName: 'cars_service',
+        options: GeneratorOptions(
+          inputFolder: '',
+          outputFolder: '',
+          ignoreHeaders: true,
+        ),
+      );
+
+      expect(result, contains('Future<chopper.Response<CarModel>>'));
+      expect(result, contains('Future<chopper.Response<CarModel>> carsGet'));
+      expect(result, contains('Future<chopper.Response<CarModel>> carsPost'));
+      expect(result, contains('Future<chopper.Response<CarModel>> carsAdditionalPut({@Body() required Object? body});'));
+    });
+  });
+}
