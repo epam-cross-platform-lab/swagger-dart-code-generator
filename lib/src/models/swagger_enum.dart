0412121a75b4864334e9a8e88fca9cacc59cf787
via-guy <74960121+via-guy@users.noreply.github.com>
2022-04-12 09:47:57 +0200

    Fix sending integer enums (#379)
    * Generate integer enum values

---
+++ lib/src/models/swagger_enum.dart
@@ -0,0 +1,31 @@
+import 'package:swagger_dart_code_generator/src/code_generators/constants.dart';
+
+class SwaggerEnum {
+  const SwaggerEnum({
+    required this.name,
+    required this.isInteger,
+  });
+
+  SwaggerEnum.fromSchema(Map<String, dynamic> schema)
+      : name = schema['name'] as String,
+        isInteger = kIntegerTypes.contains(schema['type']);
+
+  final String name;
+
+  /// Enums are either int or string
+  final bool isInteger;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SwaggerEnum &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          isInteger == other.isInteger;
+
+  @override
+  int get hashCode => name.hashCode ^ isInteger.hashCode;
+
+  @override
+  String toString() => '{name: $name, isInteger: $isInteger}';
+}
