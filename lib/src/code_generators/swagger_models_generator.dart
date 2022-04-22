7026f0d54e72644d759ba676e004f80f74cb9a1f
uladzimir_paliukhovich <>
2022-04-22 12:02:32 +0300

    Fixed generation again

--- lib/src/code_generators/swagger_models_generator.dart
+++ lib/src/code_generators/swagger_models_generator.dart
@@ -1116,7 +1116,7 @@ if(${neededName.camelCase} is String)
   
     return defaultValue ??
       enums.\$${neededName}Map.entries
-          .firstWhere((element) => element.value == defaultValue, orElse: () => enums.$neededName.swaggerGeneratedUnknown)
+          .firstWhere((element) => element.value == defaultValue, orElse: () => MapEntry(enums.$neededName.swaggerGeneratedUnknown, 0)
           .key;
 }
 
