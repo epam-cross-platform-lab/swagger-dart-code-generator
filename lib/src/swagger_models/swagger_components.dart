7499813b5b06339042fffbc547473bc160cd6156
uladzimir_paliukhovich <>
2022-05-10 13:57:46 +0300

    Fixed responses generation

--- lib/src/swagger_models/swagger_components.dart
+++ lib/src/swagger_models/swagger_components.dart
@@ -54,7 +54,14 @@ Map<String, SwaggerSchema> _mapResponses(Map<String, dynamic>? json) {
     if (content?.length == 1) {
       appJson = content?.values.first as Map<String, dynamic>?;
     } else {
-      appJson = content?['application/json'] as Map<String, dynamic>?;
+      final result = content?.entries
+          .firstWhere(
+            (element) => element.key.startsWith('application/json'),
+            orElse: () => MapEntry('', ''),
+          )
+          .value;
+
+      appJson = result as Map<String, dynamic>?;
     }
 
     if (appJson != null && appJson['schema'] != null) {
