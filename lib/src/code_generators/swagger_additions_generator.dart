0a75aab485ac64ad7f12614e40b0a4cbc734e46b
Uladzimir_Paliukhovich <Vovanella95@mail.ru>
2022-05-20 11:38:09 +0300

    Version/2.5.4 (#411)
    * Fixed mapping of classes with typedef
    
    * Some fix
    
    * Fix
    
    * Removed condition type==object for allOf items generation
    
    * Updated pubspec and changelog
    
    Co-authored-by: uladzimir_paliukhovich <>

--- lib/src/code_generators/swagger_additions_generator.dart
+++ lib/src/code_generators/swagger_additions_generator.dart
@@ -137,11 +137,20 @@ class \$CustomJsonDecoder {
   final Map<Type, \$JsonFactory> factories;
 
   dynamic decode<T>(dynamic entity) {
+
     if (entity is Iterable) {
       return _decodeList<T>(entity);
     }
 
     if (entity is T) {
+      return entity;
+    }
+
+    if (isTypeOf<T, Map>()) {
+      return entity;
+    }
+
+     if(isTypeOf<T, Iterable>()) {
       return entity;
     }
 
