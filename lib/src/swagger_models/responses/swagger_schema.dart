3c121828f3d145ed2c1cc632e5bad7af9406e297
Uladzimir Paliukhovich <uladzimir_paliukhovich@epam.com>
2023-02-17 14:58:32 +0300

    Fixed ref

--- lib/src/swagger_models/responses/swagger_schema.dart
+++ lib/src/swagger_models/responses/swagger_schema.dart
@@ -1,7 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:swagger_dart_code_generator/src/code_generators/constants.dart';
 
-part 'swagger_schema.g.dart';
+part 'swagger_schema.g2.dart';
 
 @JsonSerializable()
 class SwaggerSchema {
