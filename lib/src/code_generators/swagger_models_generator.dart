941c3416b2c98ae0cfc433d0563acb610340703c
Uladzimir_Paliukhovich <Vovanella95@mail.ru>
2023-02-28 13:45:43 +0300

    Version/2.10.2 (#559)
    * Fixed generation of nullable objects
    
    * Fixed nullable properties generation
    
    * Added 202 to success response codes
    
    * Fixed generation of enums
    
    * Fixed generation of models and enums
    
    * Refactored swagger generator
    
    * Some optimizations and refactoring
    
    * Small fix
    
    * Fixed naming of response
    
    * Fixed #531 generation of names of Part
    
    * Some fixes and refactoring
    
    * Fixed tests
    
    * Updated readme
    
    * Updated analysis options
    
    * Reverted SDK version
    
    * Updated version
    
    * Implemented Issue #496 x-ms-enum support
    
    * Some fixes
    
    * Removed flutter analyse
    
    * Added enumNames to enumModel
    
    * Returned ability to generate x-enumNames
    https://github.com/epam-cross-platform-lab/swagger-dart-code-generator/issues/539
    
    * Fixed tests
    
    * Fixed generation of multipart file type
    
    * Re-generated files
    
    * Fixed job run
    
    * Added 'Uri' to booked classes names
    
    * Fixed issue with duplicated enum field names
    
    * Pushed analysis options
    
    * Fixed case when allOf classes refer to allOf classes
    
    * Fixed Order model in example
    
    * Removed discriminator
    
    * Fixed ref
    
    * Add some improvements to enums generator
    
    * Updated pubspec and changelog
    
    * Fixed duplicated enum values
    
    * Removed not needed generated code
    
    * Fixed enums toJson generation
    
    * Fixed generation of custom model
    
    ---------
    
    Co-authored-by: Uladzimir Paliukhovich <uladzimir_paliukhovich@epam.com>

--- lib/src/code_generators/swagger_models_generator.dart
+++ lib/src/code_generators/swagger_models_generator.dart
@@ -103,6 +103,11 @@ abstract class SwaggerModelsGenerator extends SwaggerGeneratorBase {
 
         final itemClassName = '$className\$Item';
 
+        if (options.overridenModels
+            .contains(getValidatedClassName(itemClassName))) {
+          return '';
+        }
+
         final resultClass = generateModelClassString(
           root,
           itemClassName,
@@ -213,7 +218,7 @@ abstract class SwaggerModelsGenerator extends SwaggerGeneratorBase {
     required List<EnumModel> allEnums,
     required bool generateEnumsMethods,
   }) {
-    final ttt = generateEnumsMethods
+    final allEnumsString = generateEnumsMethods
         ? allEnums.map((e) => e.generateFromJsonToJson()).join()
         : '';
 
@@ -227,7 +232,7 @@ abstract class SwaggerModelsGenerator extends SwaggerGeneratorBase {
     classes.addAll(classesFromInnerClasses);
 
     if (classes.isEmpty) {
-      return '';
+      return allEnumsString;
     }
 
     var results = classes.keys.map((String className) {
@@ -260,7 +265,7 @@ abstract class SwaggerModelsGenerator extends SwaggerGeneratorBase {
       results = results.replaceAll(' $listEnum ', ' List<$listEnum> ');
     }
 
-    return results + ttt;
+    return results + allEnumsString;
   }
 
   static String getValidatedParameterName(String parameterName) {
