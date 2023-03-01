f68ef0681d1d02071d7b06432f042bf0df891503
Uladzimir_Paliukhovich <Vovanella95@mail.ru>
2023-03-01 11:52:55 +0300

    Version/2.10.2 (#561)
    * Fixed generation of nullable objects
    
    * Fixed nullable properties generation
    
    * Added 202 to success response codes
    
    * Fixed generation of enums
    
    * Fixed generation of models and enums
    
    * Refactored swagger generator
    
    * Some optimizations and refactoring
    
    * Small fix
    
    * Fixed naming of response
    
    * Fixed #531 generation of names of Part
    
    * Some fixes and refactoring
    
    * Fixed tests
    
    * Updated readme
    
    * Updated analysis options
    
    * Reverted SDK version
    
    * Updated version
    
    * Implemented Issue #496 x-ms-enum support
    
    * Some fixes
    
    * Removed flutter analyse
    
    * Added enumNames to enumModel
    
    * Returned ability to generate x-enumNames
    https://github.com/epam-cross-platform-lab/swagger-dart-code-generator/issues/539
    
    * Fixed tests
    
    * Fixed generation of multipart file type
    
    * Re-generated files
    
    * Fixed job run
    
    * Added 'Uri' to booked classes names
    
    * Fixed issue with duplicated enum field names
    
    * Pushed analysis options
    
    * Fixed case when allOf classes refer to allOf classes
    
    * Fixed Order model in example
    
    * Removed discriminator
    
    * Fixed ref
    
    * Add some improvements to enums generator
    
    * Updated pubspec and changelog
    
    * Fixed duplicated enum values
    
    * Removed not needed generated code
    
    * Fixed enums toJson generation
    
    * Fixed generation of custom model
    
    * Fixed #540 Generation of enum maps in case when no models defined
    
    * Removed not needed file
    
    ---------
    
    Co-authored-by: Uladzimir Paliukhovich <uladzimir_paliukhovich@epam.com>

--- example/lib/swagger_generated_code/client_index.dart
+++ example/lib/swagger_generated_code/client_index.dart
@@ -1,4 +1,3 @@
-export 'liberty_vod_service.swagger.dart' show LibertyVodService;
 export 'pet_service_json.swagger.dart' show PetServiceJson;
 export 'pet_service_json.swagger.dart' show PetServiceJson;
 export 'pet_service_swagger.swagger.dart' show PetServiceSwagger;
