f1ff0f1e03d76efba562b568c1f6a9d2493a7281
RB0F31CL <romain-externe.boucher@enedis.fr>
2023-04-06 17:16:48 +0200

    prefix request path with base path

--- lib/src/models/generator_options.dart
+++ lib/src/models/generator_options.dart
@@ -7,6 +7,7 @@ class GeneratorOptions {
   /// Instantiate generator options.
   GeneratorOptions({
     this.withBaseUrl = true,
+    this.addBasePathToRequests = false,
     this.withConverter = true,
     this.ignoreHeaders = false,
     this.separateModels = false,
@@ -44,6 +45,9 @@ class GeneratorOptions {
 
   @JsonKey(defaultValue: true)
   final bool withBaseUrl;
+
+  @JsonKey(defaultValue: false)
+  final bool addBasePathToRequests;
 
   @JsonKey(defaultValue: null)
   final int? pageWidth;
