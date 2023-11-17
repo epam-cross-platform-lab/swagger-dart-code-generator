8c1fe97063b86bd516cece32b1459c3beb3ff241
Uladzimir Paliukhovich <uladzimir_paliukhovich@epam.com>
2023-11-17 15:08:26 +0300

    Pushed constants file

--- lib/src/code_generators/constants.dart
+++ lib/src/code_generators/constants.dart
@@ -88,6 +88,8 @@ const kCookie = 'cookie';
 const kArray = 'array';
 const kEnum = 'enum';
 const kBody = 'body';
+const kQuery = 'query';
+const kExplodedQuery ='ExplodedQuery';
 const kPartFile = 'partFile';
 const kPart = 'part';
 
