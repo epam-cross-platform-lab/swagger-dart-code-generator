9d0e0f57e2dabd310f1ffa1de5476f981e47f734
shilangyu <xmarcinmarcin@gmail.com>
2024-01-23 13:27:16 +0100

    Bump lints

--- lib/src/code_generators/v2/swagger_enums_generator_v2.dart
+++ lib/src/code_generators/v2/swagger_enums_generator_v2.dart
@@ -1,10 +1,9 @@
 import 'package:swagger_dart_code_generator/src/code_generators/enum_model.dart';
 import 'package:swagger_dart_code_generator/src/code_generators/swagger_enums_generator.dart';
-import 'package:swagger_dart_code_generator/src/models/generator_options.dart';
 import 'package:swagger_dart_code_generator/src/swagger_models/swagger_root.dart';
 
 class SwaggerEnumsGeneratorV2 extends SwaggerEnumsGenerator {
-  SwaggerEnumsGeneratorV2(GeneratorOptions options) : super(options);
+  SwaggerEnumsGeneratorV2(super.options);
 
   @override
   String generate({
--- lib/src/code_generators/v3/swagger_enums_generator_v3.dart
+++ lib/src/code_generators/v3/swagger_enums_generator_v3.dart
@@ -1,11 +1,10 @@
 import 'package:swagger_dart_code_generator/src/code_generators/enum_model.dart';
 import 'package:swagger_dart_code_generator/src/code_generators/swagger_enums_generator.dart';
 import 'package:swagger_dart_code_generator/src/code_generators/v2/swagger_models_generator_v2.dart';
-import 'package:swagger_dart_code_generator/src/models/generator_options.dart';
 import 'package:swagger_dart_code_generator/src/swagger_models/swagger_root.dart';
 
 class SwaggerEnumsGeneratorV3 extends SwaggerEnumsGenerator {
-  SwaggerEnumsGeneratorV3(GeneratorOptions options) : super(options);
+  SwaggerEnumsGeneratorV3(super.options);
 
   @override
   String generate({
