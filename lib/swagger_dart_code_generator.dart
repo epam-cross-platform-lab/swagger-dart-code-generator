55fe1191d337c1e2f462198c5657a87fe2bfa11f
Dmytro Shevchenko <162686810+DmytroShevchenkoR@users.noreply.github.com>
2024-10-29 17:56:13 +0100

    Fix missing 'import_paths' for Scalars when 'separate_models' is enabled

--- lib/swagger_dart_code_generator.dart
+++ lib/swagger_dart_code_generator.dart
@@ -333,7 +333,7 @@ $dateToJson
 import 'package:json_annotation/json_annotation.dart';
 import 'package:collection/collection.dart';
 import 'dart:convert';
-
+${options.importPaths.map((e) => "import '$e';").join('\n')}
 $enumsImport
 $overridenModels
 
