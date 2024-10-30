010d57fc0689bfb4d2d6bdc50609fd43844da3b1
Anna Kandel <anna@arnac.io>
2024-10-30 12:30:45 +0200

    BREAKING: Add Chopper 8 support (#755)

--- example/lib/swagger_generated_code/some_file_name.swagger.g.dart
+++ example/lib/swagger_generated_code/some_file_name.swagger.g.dart
@@ -1,10 +1,39 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'pet_service_json.swagger.dart';
+part of 'some_file_name.swagger.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
+
+Order _$OrderFromJson(Map<String, dynamic> json) => Order(
+      id: (json['id'] as num?)?.toInt(),
+      petId: (json['petId'] as num?)?.toInt(),
+      quantity: (json['quantity'] as num?)?.toInt(),
+      shipDate: json['shipDate'] == null
+          ? null
+          : DateTime.parse(json['shipDate'] as String),
+      status: orderStatusNullableFromJson(json['status']),
+      complete: json['complete'] as bool? ?? false,
+    );
+
+Map<String, dynamic> _$OrderToJson(Order instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  writeNotNull('petId', instance.petId);
+  writeNotNull('quantity', instance.quantity);
+  writeNotNull('shipDate', instance.shipDate?.toIso8601String());
+  writeNotNull('status', orderStatusNullableToJson(instance.status));
+  writeNotNull('complete', instance.complete);
+  return val;
+}
 
 Category _$CategoryFromJson(Map<String, dynamic> json) => Category(
       id: (json['id'] as num?)?.toInt(),
@@ -72,6 +101,39 @@ Map<String, dynamic> _$TagToJson(Tag instance) {
 
   writeNotNull('id', instance.id);
   writeNotNull('name', instance.name);
+  return val;
+}
+
+Pet _$PetFromJson(Map<String, dynamic> json) => Pet(
+      id: (json['id'] as num?)?.toInt(),
+      category: json['category'] == null
+          ? null
+          : Category.fromJson(json['category'] as Map<String, dynamic>),
+      name: json['name'] as String? ?? '',
+      photoUrls: (json['photoUrls'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      tags: (json['tags'] as List<dynamic>?)
+          ?.map((e) => Tag.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      status: petStatusNullableFromJson(json['status']),
+    );
+
+Map<String, dynamic> _$PetToJson(Pet instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  writeNotNull('category', instance.category?.toJson());
+  writeNotNull('name', instance.name);
+  writeNotNull('photoUrls', instance.photoUrls);
+  writeNotNull('tags', instance.tags?.map((e) => e.toJson()).toList());
+  writeNotNull('status', petStatusNullableToJson(instance.status));
   return val;
 }
 
